626c5952b5ac9421038ef53d10f8e5c43af0ad12
Vinita <vinita.g@prarang.in>
2024-10-25 18:28:32 +0530

    25 code

---
+++ Modules/YellowPages/database/migrations/2024_10_25_062333_create_social_media_platforms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $connection = 'yp';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_media_platforms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique(); 
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_media_platforms');
+    }
+};
