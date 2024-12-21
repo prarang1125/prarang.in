e3d3051f83bd2f5db1f249bc33dfc8babdf886cd
ASG Kali <dev.vivek16@gmail.com>
2024-12-21 17:26:44 +0530

    new

--- resources/views/portal/post-summary.blade.php
+++ resources/views/portal/post-summary.blade.php
@@ -332,6 +332,7 @@
 
     <x-post.footer :city="$city_name" />
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> --}}
 </body>
 
