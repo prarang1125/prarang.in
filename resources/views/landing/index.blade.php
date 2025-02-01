d91347502bf5b6817441f2bcc2ffd593d81500ca
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-02-01 15:45:15 +0530

    old

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Meerut YellowPags</title>
+    <title>मेरठ येलो पेज</title>
+    {{-- meta data tags for facebook --}}
+    <meta property="og:title" content="मेरठ येलो पेज">
+    <meta property="og:description" content="मेरठ में व्यवसायों और सेवाओं की जानकारी को बढ़ाएं। अपने व्यवसाय को मुफ्त में ऑनलाइन (Online) करे">
+    <meta property="og:image" content="{{ asset('assets/images/meerut_yp_metaimg.webp') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
     <script src="https://cdn.tailwindcss.com"></script>
 
 
