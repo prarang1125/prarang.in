a6225dd5afa2acb336f0155e5b61f364ad55e45f
Vinita <vinita.g@prarang.in>
2025-02-26 17:04:26 +0530

    newcode

--- Modules/YellowPages/Http/Controllers/VCard/CreateVCardController.php
+++ Modules/YellowPages/Http/Controllers/VCard/CreateVCardController.php
@@ -29,7 +29,6 @@ class CreateVCardController extends Controller
 
     public function store(StoreVCardRequest $request)
     {
-       
         $validatedData = $request->validated();
     
         // Handle file uploads
@@ -141,7 +140,6 @@ class CreateVCardController extends Controller
                 'vcard', 'vcardInfo', 'categories', 'cities', 'dynamicFields', 'address', 'user'
             ));
         } catch (Exception $e) {
-            // dd($e->getMessage());
             Log::error('Error editing VCard: ' );
             return redirect()->back()->withErrors(['error' => 'Unable to fetch VCard details for editing.']);
         }
@@ -350,10 +348,9 @@ class CreateVCardController extends Controller
         if (!$vcard) {
             abort(404, 'vCard not found.');
         }
-        
+    
         // Increment the scan_count column
         $count =$vcard->increment('scan_count');
-        return redirect()->route('vCard.share', ['slug' => $vcard->slug]);
     
         // Load the user with their address using the hasOne relationship.
         $user = User::with('address')->find($vcard->user_id);
