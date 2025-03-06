e85ee174b7c47827c4b631f52338e6200d9fa994
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-03-06 09:58:32 +0530

    version_v1.0 [MISC] 06-Mar-2025 DESKTOP-DIAH5OB ASG_Kali(AVR) New:0 Edit:1 Del:0  Old

--- Modules/YellowPages/resources/views/layout/auth.blade.php
+++ Modules/YellowPages/resources/views/layout/auth.blade.php
@@ -5,21 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>@yield('title', 'Yellow Pages - Find Businesses & Services')</title>
+    <title>@yield('title', 'देश का पहला हिंदी येलोपेज़!')</title>
 
-    <meta name="description" content="@yield('meta_og_description', 'Discover local businesses, services, and more.')">
+    <meta name="description" content="@yield('meta_og_description', 'देश के पहले हिंदी येलोपेज़ पर अपना वेबपेज बनाएं और अपना बिज़नेस कार्ड प्रिंट करें!')">
     <meta name="keywords" content="@yield('meta_keywords', 'yellow pages, local directory, businesses')">
 
     <!-- Open Graph Meta Tags for SEO & Social Sharing -->
     <meta property="og:locale" content="en_IN">
     <meta name="robots" content="index, follow">
     <meta property="og:type" content="article">
-    <meta property="og:image:width" content="600">
-    <meta property="og:image:height" content="315">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="628">
     <meta property="og:site_name" content="Yellow Pages">
 
-    <meta property="og:title" content="@yield('meta_og_title', 'Yellow Pages - Business Directory')">
-    <meta property="og:description" content="@yield('meta_og_description', 'Find the best businesses and services near you.')">
+    <meta property="og:title" content="@yield('meta_og_title', 'देश का पहला हिंदी येलोपेज़!')">
+    <meta property="og:description" content="@yield('meta_og_description', 'देश के पहले हिंदी येलोपेज़ पर अपना वेबपेज बनाएं और अपना बिज़नेस कार्ड प्रिंट करें!')">
     <meta property="og:image" content="@yield('meta_og_image', 'https://i.ibb.co/tpxh7Vcf/Lucknow-yp-1200-630-still.png')">
     <meta property="og:url" content="{{ url()->current() }}">
 
