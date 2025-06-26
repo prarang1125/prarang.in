788318cc021aad9d4dd6d09e727e5c2120a7c306
Vinita <vinita.g@prarang.in>
2025-06-26 09:34:21 +0530

    newcode

--- resources/views/livewire/pages/upmana-ai.blade.php
+++ resources/views/livewire/pages/upmana-ai.blade.php
@@ -445,7 +445,6 @@
                                                             @endforeach
                                                             <hr>
                                                         @endforeach
-
                                                     </div>
 
                                                 </div>
