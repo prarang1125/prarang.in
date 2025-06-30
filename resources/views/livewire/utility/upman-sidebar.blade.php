ba1215c6ae7fd1f8aa532ad36c0b0ea17f057ee4
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-06-30 10:16:51 +0530

    master [MASTER] 30-Jun-2025 DESKTOP-DIAH5OB ASG_Kali(AVR) New:2 Edit:1 Del:0  final

---
+++ resources/views/livewire/utility/upman-sidebar.blade.php
@@ -0,0 +1,86 @@
+<div>
+    <style>
+        /* Col 2 */
+        .first-prompt .firstPrompt .col-sm-2 {
+            padding-left: 0px;
+            padding-right: 0px;
+            transform: translatex(0px) translatey(0px);
+        }
+
+        /* Border dark */
+        .first-prompt div .border-dark {
+            padding-left: 7px !important;
+            padding-right: 4px !important;
+            padding-top: 4px !important;
+            padding-bottom: 4px !important;
+            width: 183px;
+            text-align: left;
+            font-size: 15px;
+            margin-bottom: 21px;
+            border-style: none !important;
+            box-shadow: 0px 0px 5px 0px #212529;
+            position: relative;
+            top: 1px;
+            transform: translatex(0px) translatey(0px);
+        }
+
+        /* Division */
+        .first-prompt .firstPrompt .row .col-sm-2>div {
+            position: relative;
+            top: -90px;
+        }
+
+        /* List Item */
+        .first-prompt ul li {
+            text-decoration: none;
+        }
+
+        /* Link */
+        .first-prompt ul a {
+            text-decoration: none;
+        }
+
+        @media (min-width:768px) {
+
+            /* Border dark */
+            .first-prompt div .border-dark {
+                display: block !important;
+            }
+
+        }
+
+        /* Link */
+        .first-prompt ul a {
+            color: #024abe;
+        }
+
+        /* Link (hover) */
+        .first-prompt ul a:hover {
+            color: #031d90;
+            font-weight: bold;
+        }
+    </style>
+    <div class="p-3 border border-dark rounded bg-light">
+        <ul class="ps-3" style="list-style: none;">
+            <li><a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#whatIsUpmanaModal">Comparison A.I.</a></li>
+            <li><a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#aiVsAgiModal">Prompt </a></li>
+            <li><a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#upmanabenefitsModal">Comparison A.I & Artificial General Intelligence (AGI) </a></li>
+            <li><a href="{{ route('insight') }}">Intelligent Insights</a></li>
+        </ul>
+    </div>
+
+    <div class="p-3 border border-dark rounded bg-light">
+        <ul class="ps-3" style="list-style: none;">
+            <li><a target="_blank" href="https://g2c.prarang.in/ai/world">World AI Report</a></li>
+            <li><a target="_blank" href="https://g2c.prarang.in/ai/india">India AI Report</a></li>
+            <br>
+            <li><a target="_blank" href="https://g2c.prarang.in/world/development-planner">World Development Planner</a></li>
+            <li><a target="_blank" href="https://g2c.prarang.in/world/market-planner">World Market Planner</a></li>
+            <li><a target="_blank" href="https://g2c.prarang.in/india/development-planners">India Development Planner</a></li>
+            <li><a target="_blank" href="https://g2c.prarang.in/india/market-planner/states">India Market Planner</a></li>
+            <!-- <li><a href="gc2.prarang.in/">India City Sentiment Analysis</a></li> -->
+        </ul>
+    </div>
+
+
+</div>
