3bef153303a8b2650f74958f9aaea9182af9f477
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-07-07 13:15:03 +0530

    master [MASTER] 07-Jul-2025 DESKTOP-DIAH5OB ASG_Kali(AVR) New:0 Edit:1 Del:0  old

--- Modules/YellowPages/resources/views/admin/checker-card-list.blade.php
+++ Modules/YellowPages/resources/views/admin/checker-card-list.blade.php
@@ -2,20 +2,20 @@
 @section('title', 'Vcard')
 
 @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
 @endif
 
 @section('content')
 <!--start page wrapper -->
 <div class="page-content">
     <!--breadcrumb-->
-    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+    <div class="mb-3 page-breadcrumb d-none d-sm-flex align-items-center">
         <div class="breadcrumb-title pe-3">वीकार्ड</div>
         <div class="ps-3">
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb mb-0 p-0">
+                <ol class="p-0 mb-0 breadcrumb">
                     <li class="breadcrumb-item"><a href="#"><i class="bx bx-user"></i></a>
                     </li>
                     <li class="breadcrumb-item active" aria-current="page">वीकार्ड लिस्टिंग</li>
@@ -23,19 +23,19 @@
             </nav>
         </div>
     </div>
-    
+
     <!--end breadcrumb-->
     <div class="row">
-        <div class="col-xl-9 mx-auto w-100">
-            
+        <div class="mx-auto col-xl-9 w-100">
+
             <!-- Success Message -->
             @if(session('success'))
-                <div class="alert alert-success mt-3">
-                    {{ session('success') }}
-                </div>
+            <div class="mt-3 alert alert-success">
+                {{ session('success') }}
+            </div>
             @endif
             <h6 class="mb-0 text-uppercase">वीकार्ड लिस्टिंग</h6>
-            <hr/>
+            <hr />
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-end"> <!-- Align to right -->
@@ -46,7 +46,7 @@
                             </div>
                         </form>
                     </div>
-                    
+
                     <table class="table mb-0 table-hover">
                         <thead>
                             <tr>
@@ -63,33 +63,33 @@
                             @php $index = 1; @endphp
                             @foreach($Vcard_list as $vcard)
                             {{-- {{dd($vcard);}} --}}
-                                <tr>
-                                    <th scope="row" class="align-middle">{{ $index }}</th> 
-                                    <td class="align-middle">{{ $vcard->user->name }}</td>
-                                    <td class="align-middle">
-                                     {{ $vcard->user && $vcard->user->name ? $vcard->user->name : 'N/A' }}
-                                    </td>
-                                    <td class="align-middle">{{ $vcard->color_code }}</td> 
-                                    <td class="align-middle">
-                                        <img src="{{ $vcard->user->profile ? Storage::url($vcard->user->profile) : asset('images/default-profile.png') }}" 
-                                             alt="Profile Image" 
-                                             style="max-width: 100px;">
-                                    </td>                                    
-                                    <td class="align-middle">{{ $vcard->scan_count }}</td>                         
-                                    <td class="align-middle">{{ $vcard->created_at}}</td>
-                                    <td class="align-middle">
-                                        @if($vcard->is_active == 1)
-                                            <a href="{{ route('checker.Card-approve', $vcard->id) }}" class="btn btn-sm btn-success edit-user">
-                                                स्वीकृत
-                                            </a>
-                                        @else
-                                            <a href="{{ route('checker.Card-approve', $vcard->id) }}" class="btn btn-sm btn-danger edit-user">
-                                                देखे
-                                            </a>
-                                        @endif
-                                    </td>
-                                </tr>
-                                @php $index++; @endphp
+                            <tr>
+                                <th scope="row" class="align-middle">{{ $index }}</th>
+                                <!-- <td class="align-middle">{{ $vcard->user->name ?? '' }}</td> -->
+                                <td class="align-middle">
+                                    {{ $vcard->user && $vcard->user->name ? $vcard->user->name : 'N/A' }}
+                                </td>
+                                <td class="align-middle">{{ $vcard->color_code }}</td>
+                                <td class="align-middle">
+                                    <img src="{{ $vcard->user->profile ? Storage::url($vcard->user->profile) : asset('images/default-profile.png') }}"
+                                        alt="Profile Image"
+                                        style="max-width: 100px;">
+                                </td>
+                                <td class="align-middle">{{ $vcard->scan_count }}</td>
+                                <td class="align-middle">{{ $vcard->created_at}}</td>
+                                <td class="align-middle">
+                                    @if($vcard->is_active == 1)
+                                    <a href="{{ route('checker.Card-approve', $vcard->id) }}" class="btn btn-sm btn-success edit-user">
+                                        स्वीकृत
+                                    </a>
+                                    @else
+                                    <a href="{{ route('checker.Card-approve', $vcard->id) }}" class="btn btn-sm btn-danger edit-user">
+                                        देखे
+                                    </a>
+                                    @endif
+                                </td>
+                            </tr>
+                            @php $index++; @endphp
                             @endforeach
                         </tbody>
                     </table>
@@ -99,7 +99,3 @@
     </div>
 </div>
 @endsection
-
-
-
-
