ad7540812f52de806d562f63aa83e2a1334c4372
ASG Kali new <vivek.k@prarang.in>
2026-04-17 15:04:46 +0530

    old

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,20 @@
-@extends('errors::minimal')
+<x-layout.main.base>
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Error On Page</div>
+
+                    <div class="card-body">
+                        <h1 class="text-4xl font-bold text-red-600">Error On Page </h1>
+                        <p class="mt-4 text-lg text-gray-700">Sorry, something went wrong on our end. Please try again later.</p>
+                        <a href="{{ url('/') }}" class="mt-6 inline-block bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">Go Back Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-layout.main.base>
