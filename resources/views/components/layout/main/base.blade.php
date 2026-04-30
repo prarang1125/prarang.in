45cbec1cbaa2569e4b6c8938be0314b2f0bf068c
ASG Kali new <vivek.k@prarang.in>
2026-04-30 15:55:54 +0530

    new

--- resources/views/components/layout/main/base.blade.php
+++ resources/views/components/layout/main/base.blade.php
@@ -874,13 +874,14 @@
                                                 href="https://g2c.prarang.in/planners/market">Market
                                                 Planner</a>
                                         </li>
+                                        <li><a class="dropdown-item" href="https://g2c.prarang.in/planners">Cyber Risk
+                                                Analyser
+                                            </a></li>
                                         <li><a class="dropdown-item"
                                                 href="https://g2c.prarang.in/planners/communication">Comunication
                                                 Planner</a>
                                         </li>
-                                        <li><a class="dropdown-item" href="https://g2c.prarang.in/planners">Cyber Risk
-                                                Analyser
-                                            </a></li>
+
 
                                     </ul>
                                 </li>
